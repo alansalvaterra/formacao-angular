9efd08abc4f6fa7d7bc1c490baa991e769b0107e
Miguel Salvaterra <alan.salvaterra@gmail.com>
2024-03-21 00:53:47 -0300

    Aula 5 - Angular 14: Aplique conceitos e desenvolva seu primeiro CRUD

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,18 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum iaculis tincidunt orci, at dignissim elit ultricies vel. Cras nulla justo, sagittis a leo euismod, ultrices porta lectus. Nam ut egestas nisl. Vestibulum scelerisque elit quis tortor eleifend.',
-      autoria: 'Alan',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum iaculis tincidunt orci, at dignissim elit ultricies vel. Nam ut egestas nisl. Vestibulum scelerisque elit quis tortor eleifend.',
-      autoria: 'Alan Salvaterra',
-      modelo: 'modelo1'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
