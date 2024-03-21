7df160dd6c6d2911827125cdb43a4266f8835bd5
Miguel Salvaterra <alan.salvaterra@gmail.com>
2024-03-20 23:10:41 -0300

    Aula 4 - Angular 14: Aplique conceitos e desenvolva seu primeiro CRUD

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum iaculis tincidunt orci, at dignissim elit ultricies vel. Cras nulla justo, sagittis a leo euismod, ultrices porta lectus. Nam ut egestas nisl. Vestibulum scelerisque elit quis tortor eleifend.',
+      autoria: 'Alan',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum iaculis tincidunt orci, at dignissim elit ultricies vel. Nam ut egestas nisl. Vestibulum scelerisque elit quis tortor eleifend.',
+      autoria: 'Alan Salvaterra',
+      modelo: 'modelo1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
